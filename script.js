d6c25bf4be17d24386a4fa5b864df65b9db5e5b7
atharva-bhatnagar <87374523+atharva-bhatnagar@users.noreply.github.com>
2023-01-13 15:21:22 +0530

    added football ques sol

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ let obj={
     rollno:30
 }
 console.log(obj)
+
+/*Petya loves football very much. One day, as he was watching a football match, he was writing the 
+players' current positions on a piece of paper. To simplify the situation he depicted it as a string consisting of
+ zeroes and ones. A zero corresponds to players of one team; a one corresponds to players of another team. If 
+there are at least 7 players of some team standing one after another, then the situation is considered dangerous. 
+For example, the situation 00100110111111101 is dangerous and 11110111011101 is not. You are given the current situation. Determine whether it is dangerous or not.
+
+The first input line contains a non-empty string consisting of characters "0"
+ and "1", which represents players. The length of the string does not exceed 100 characters. There's at least one player from each team present on the field.
+
+Print "YES" if the situation is dangerous. Otherwise, print "NO".8*/
+
+let playerStr="11010100100000000011"
+
+if(playerStr.includes("1111111")||playerStr.includes("0000000")){
+    console.log("yes")
+}else{
+    console.log("no")
+}
